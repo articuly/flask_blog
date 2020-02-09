9b8b1f21bf8b56083002b759bcd5341bff7a7820
articuly <articuly@gmail.com>
2020-02-09 23:29:21 +0800

    完善导航条，增加富文本编辑器功能

--- static/ckeditor/config.js
+++ static/ckeditor/config.js
@@ -5,6 +5,6 @@
 
 CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
-	// config.language = 'fr';
+	config.language = 'zh-cn';
 	// config.uiColor = '#AADC6E';
 };
